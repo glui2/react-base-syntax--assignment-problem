8b62956b39750f6c13d798eb16eae0a1b68f9ad1
Gavin <gavin.lui@gmail.com>
2020-01-24 13:39:55 +1100

    Added basic stylings

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./UserInput.css";
 
 const userInput = props => {
   return (
-    <div>
+    <div className="Input">
       <input
         type="text"
         onChange={props.changed}
--- src/components/UserOutput.js
+++ src/components/UserOutput.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./UserOutput.css";
 
 const userOutput = props => {
   return (
-    <div>
+    <div className="Output">
       <p>This is paragraph 1, written by {props.username}</p>
       <p>This is paragraph 2</p>
     </div>
